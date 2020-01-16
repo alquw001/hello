c1397b3e834aa0cb61d5725ba21fde3d84eee3ba
malbarwa <mosen9736@gmail.com>
2020-01-16 13:50:25 -0600

    Update hello.c
    I added my name.

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main (void){
  printf("Asama AL-Quwaitai");
- printf("Mechanical Engineering"); 
+ printf("Mechanical Engineering");
+ printf("Mohsin Al barwani\n", );
 return 0;
 }
